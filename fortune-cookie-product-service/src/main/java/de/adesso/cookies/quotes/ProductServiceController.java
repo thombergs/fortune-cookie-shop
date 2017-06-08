a07085bae1569fdc0507e2bb7861057e7204fcf9
mmueller <mmueller@mmueller-VirtualBox>
2017-06-08 14:52:23 +0200

    revised REST-Service for listing fortune cookies

--- fortune-cookie-product-service/src/main/java/de/adesso/cookies/quotes/ProductServiceController.java
+++ fortune-cookie-product-service/src/main/java/de/adesso/cookies/quotes/ProductServiceController.java
@@ -14,54 +14,23 @@ import java.util.Random;
 @RequestMapping("/product")
 public class ProductServiceController{
 
-    private final Random random = new Random();
+    private FortuneCookieListGenerator cookieListGenerator = new FortuneCookieListGenerator();
 
     private Logger logger = LoggerFactory.getLogger(ProductServiceController.class);
 
     @RequestMapping(method = RequestMethod.GET,path = "/fortuneCookieList", produces= MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ArrayList<FortuneCookieResource> fortuneCookieList() {
 
-        feelingLucky();
-        takeYourTime();
-
-        FortuneCookieList cookieListGenerator = new FortuneCookieList();
         ArrayList<FortuneCookieResource> cookieList = cookieListGenerator.generateList(20);
 
-        logger.info("FortuneCookieList sent successfully!");
+        logger.info("FortuneCookieListGenerator sent successfully!");
 
         return cookieList;
-
-    }
-
-    private void feelingLucky() {
-        Double percentage = random.nextDouble();
-        if (percentage < 0.3) {
-            throw new RuntimeException("Ouch!!!");
-        }
-    }
-
-    private void takeYourTime() {
-        int milliseconds = random.nextInt(5000);
-        try {
-            Thread.sleep(milliseconds);
-        }
-        catch (InterruptedException e) {
-            throw new RuntimeException("BWAAAAAH!", e);
-        }
-
-        if (milliseconds > 4500) {
-            try {
-                Thread.sleep(600000);
-            }
-            catch (InterruptedException e) {
-                throw new RuntimeException("BOOOOH!", e);
-            }
-        }
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handleException(final RuntimeException e) {
-        logger.error("Cannot sent FortuneCookieList due to error!", e);
+        logger.error("Cannot sent FortuneCookieListGenerator due to error!", e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
