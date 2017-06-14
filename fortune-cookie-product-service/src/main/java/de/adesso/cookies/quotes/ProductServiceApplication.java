a8846649b605e2267e12819bf2c76764297dfbb1
mmueller <mmueller@mmueller-VirtualBox>
2017-06-14 14:41:56 +0200

    added hystrix-dashboard compatibility

--- fortune-cookie-product-service/src/main/java/de/adesso/cookies/quotes/ProductServiceApplication.java
+++ fortune-cookie-product-service/src/main/java/de/adesso/cookies/quotes/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package de.adesso.cookies.quotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
